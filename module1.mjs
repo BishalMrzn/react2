4dd9f355dfafbd9a8c70cec9bbfd8584f569d66f
BishalMrzn <hapiebishal5@gmail.com>
2024-08-17 23:02:38 +0545

    idk

---
+++ module1.mjs
@@ -0,0 +1,5 @@
+import dz,{a,b,d}from './module2.mjs'
+console.log(dz);
+console.log(d);
+console.log(b);
+console.log(a);
---
+++ module2.mjs
@@ -0,0 +1,10 @@
+const a="Bishal";
+const b="Ranjit";
+const c="Saroj";
+const d="Bhandari";
+
+// export default a;
+export default c;
+export{a}
+export{b}
+export{d}
